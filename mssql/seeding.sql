b280d180a2e07add4a85e00c1cfacd5b3780dce2
Stuart Creed <stuart.creed@poolebayholdings.co.uk>
2022-12-28 21:15:02 +0000

    updates seeding

--- mssql/seeding.sql
+++ mssql/seeding.sql
@@ -11,10 +11,12 @@ CREATE table Companies (
 create table Users (
     UserId int IDENTITY primary key,
     FirstName VARCHAR(20),
+    LastName VARCHAR(20),
     Company int REFERENCES Companies(CompanyId)
 )
 
--- insert into Users values ('Bob', 1);
+insert into Companies values ('Airbus'), ('Esso'), ('Poole Bay Holdings')
+insert into Users values ('Bob', 'Smith', 1), ('Sam', 'Smith', 1), ('Alex', 'Yakens', 2), ('Stuart', 'Creed', 3)
+insert into Users values ('Luke', 'Eurke', null)
 
-select * from Users
-select * from Companies
+select FirstName as "First Name", LastName as "Last Name", CompanyName as "Company Name" from Users left join Companies on Users.Company = Companies.CompanyId
