c0722ae1a3f2024ed6a5bdcd85764ad021a5db28
Stuart Creed <stuart.creed@poolebayholdings.co.uk>
2022-12-31 11:58:01 +0000

    Adds draw io and more seeding changes

--- mssql/seeding.sql
+++ mssql/seeding.sql
@@ -3,20 +3,34 @@ drop table Users
 drop table Companies
 
 
-CREATE table Companies (
+CREATE table Companies
+(
     CompanyId int IDENTITY primary key,
     CompanyName VARCHAR(100)
 )
 
-create table Users (
+create table Users
+(
     UserId int IDENTITY primary key,
     FirstName VARCHAR(20),
     LastName VARCHAR(20),
     Company int REFERENCES Companies(CompanyId)
 )
 
-insert into Companies values ('Airbus'), ('Esso'), ('Poole Bay Holdings')
-insert into Users values ('Bob', 'Smith', 1), ('Sam', 'Smith', 1), ('Alex', 'Yakens', 2), ('Stuart', 'Creed', 3)
-insert into Users values ('Luke', 'Eurke', null)
+insert into Companies
+values
+    ('Airbus'),
+    ('Esso'),
+    ('Poole Bay Holdings')
+insert into Users
+values
+    ('Bob', 'Smith', 1),
+    ('Sam', 'Smith', 1),
+    ('Alex', 'Yakens', 2),
+    ('Stuart', 'Creed', 3)
+insert into Users
+values
+    ('Luke', 'Eurke', null)
 
-select FirstName as "First Name", LastName as "Last Name", CompanyName as "Company Name" from Users left join Companies on Users.Company = Companies.CompanyId
+select FirstName as "First Name", LastName as "Last Name", CompanyName as "Company Name"
+from Users left join Companies on Users.Company = Companies.CompanyId
